ece5db43155f9c7c27bc2f894cf1194fa7bf7ac3
Aashish Khubchandani <ak7164@nyu.edu>
2024-11-30 15:17:43 -0600

    update to use continuous features and binary targets

--- dslc_documentation/functions/helper_fns.py
+++ dslc_documentation/functions/helper_fns.py
@@ -1,7 +1,10 @@
 #TODO: COPY OVER BIG FUNCTIONS FROM VARIOUS NOTEBOOKS!
 import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-def featurize_time_series(df, period_chosen, num_trailing_periods, reduce_rows=True):
+def featurize_time_series(df, period_chosen, num_trailing_periods, reduce_rows=True, set_threshold_for_target_var_bps=None):
     '''
     Create features for forecasting active returns based on specified periods.
 
@@ -30,6 +33,10 @@ def featurize_time_series(df, period_chosen, num_trailing_periods, reduce_rows=T
         If True, reduces the DataFrame to keep only the first business day of the week,
         month, quarter, or year based on the selected period. False means it keeps all rows.
         Defaults to True.
+    
+    set_threshold_for_target_var_bps : float, optional
+        If not None, the function will set the target variable to 1 if it is greater than 
+        the threshold and 0 otherwise. Defaults to None
 
     Returns:
     --------
@@ -65,4 +72,30 @@ def featurize_time_series(df, period_chosen, num_trailing_periods, reduce_rows=T
     for i in range(1, num_trailing_periods + 1):
         new_df[f'ar_{period_chosen}_t_minus_{i}'] = new_df.groupby('Ticker')[f'ar_{period_chosen}_t'].shift(i)
     new_df = new_df.dropna() #insufficient trailing periods
+    if set_threshold_for_target_var_bps is not None:
+        threshold_float = set_threshold_for_target_var_bps / 10000
+        new_df[f'ar_{period_chosen}_t'] = (new_df[f'ar_{period_chosen}_t'] > threshold_float).astype(int)
     return new_df    
+
+def evaluate_model_performance(y_eval, y_pred, y_pred_proba, PREDICTION_PERIOD, NUM_FEATURES, plot_confusion_matrix=True):
+    accuracy = accuracy_score(y_eval, y_pred)
+    precision = precision_score(y_eval, y_pred)
+    recall = recall_score(y_eval, y_pred)
+    f1 = f1_score(y_eval, y_pred)
+    roc_auc = roc_auc_score(y_eval, y_pred_proba[:, 1])
+    
+    print("Accuracy:", accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F1:", f1)
+    print("ROC AUC:", roc_auc)
+
+    if plot_confusion_matrix:
+        # Show the confusion matrix in a plot using seaborn
+        cm = confusion_matrix(y_eval, y_pred)
+        sns.heatmap(cm, annot=True, fmt='d')
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        # Print title that mentions the prediction period and num features
+        plt.title('Confusion Matrix for ' + PREDICTION_PERIOD + " prediction using " + str(NUM_FEATURES) + " Features")
+        plt.show()    
