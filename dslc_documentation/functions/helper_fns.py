2a0c21aa8b66b5cdca3c349d5ecc3c4d3acf04dd
Aashish Khubchandani <ak7164@nyu.edu>
2024-12-01 16:39:55 -0500

    +helper fns

--- dslc_documentation/functions/helper_fns.py
+++ dslc_documentation/functions/helper_fns.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+from .constants import TEST_START_DATE, EVAL_START_DATE, N_THRESHOLD_BPS, DATA_DIR, BM_NAME
 
 def featurize_time_series(df, period_chosen, num_trailing_periods, reduce_rows=True, set_threshold_for_target_var_bps=None):
     '''
@@ -84,14 +85,15 @@ def evaluate_model_performance(y_eval, y_pred, y_pred_proba, PREDICTION_PERIOD, 
     f1 = f1_score(y_eval, y_pred)
     roc_auc = roc_auc_score(y_eval, y_pred_proba[:, 1])
     
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("F1:", f1)
-    print("ROC AUC:", roc_auc)
+    print(f"Accuracy  : {accuracy:.3f}")
+    print(f"Precision : {precision:.3f}")
+    print(f"Recall    : {recall:.3f}")
+    print(f"F1        : {f1:.3f}")
+    print(f"ROC AUC   : {roc_auc:.3f}")
 
     if plot_confusion_matrix:
-        # Show the confusion matrix in a plot using seaborn
+        # Show the confusion matrix in a plot using seaborn. Make the plot small
+        plt.figure(figsize=(3, 3))
         cm = confusion_matrix(y_eval, y_pred)
         sns.heatmap(cm, annot=True, fmt='d')
         plt.xlabel('Predicted')
@@ -99,3 +101,39 @@ def evaluate_model_performance(y_eval, y_pred, y_pred_proba, PREDICTION_PERIOD, 
         # Print title that mentions the prediction period and num features
         plt.title('Confusion Matrix for ' + PREDICTION_PERIOD + " prediction using " + str(NUM_FEATURES) + " Features")
         plt.show()    
+
+def get_X_and_y_values(active_returns_df, PREDICTION_PERIOD, NUM_FEATURES):
+    df = featurize_time_series(active_returns_df, PREDICTION_PERIOD, NUM_FEATURES,set_threshold_for_target_var_bps=N_THRESHOLD_BPS)  # noqa: F405
+    target_var = "ar_" + PREDICTION_PERIOD + "_t"
+    df.reset_index(inplace=True,drop=True)
+
+    df_train_and_eval = df[(df['Date'] < TEST_START_DATE)]
+    df_test = df[(df['Date'] >= TEST_START_DATE)]
+    df_train = df[(df['Date'] < EVAL_START_DATE)]
+    df_eval = df[(df['Date'] >= EVAL_START_DATE) & (df['Date'] < TEST_START_DATE)]
+
+    X_train_and_eval = df_train_and_eval.drop(columns=[target_var, "Date", "Ticker"]).to_numpy()
+    y_train_and_eval = df_train_and_eval[[target_var]].to_numpy()
+
+    X_train = df_train.drop(columns=[target_var, "Date", "Ticker"]).to_numpy()
+    y_train = df_train[[target_var]].to_numpy()
+
+    X_eval = df_eval.drop(columns=[target_var, "Date", "Ticker"])#.to_numpy()
+    y_eval = df_eval[[target_var]].to_numpy()
+
+    X_test = df_test.drop(columns=[target_var, "Date", "Ticker"]).to_numpy()
+    y_test = df_test[[target_var]].to_numpy()
+
+    # print("X_train shape:", X_train.shape)
+    # print("X_eval shape:", X_eval.shape)
+    # print("X_test shape:", X_test.shape)
+    # print("y_train shape:", y_train.shape)
+    # print("y_eval shape:", y_eval.shape)
+    # print("y_test shape:", y_test.shape)
+    return (X_train, y_train, X_eval, y_eval, X_train_and_eval, y_train_and_eval, X_test, y_test, df_train, df_eval, df_train_and_eval, df_test)         
+
+def load_active_returns():
+    active_returns_path = DATA_DIR + BM_NAME + "_active_returns.csv"
+    active_returns = pd.read_csv(active_returns_path, index_col=0, parse_dates=True)
+    print("Loaded active returns from", active_returns_path)
+    return active_returns
