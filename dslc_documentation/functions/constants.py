f0b1c52ee83e41f1e7e4ff5041e03d438f28ac30
Aashish Khubchandani <ak7164@nyu.edu>
2024-12-01 19:00:55 -0500

    period change

--- dslc_documentation/functions/constants.py
+++ dslc_documentation/functions/constants.py
@@ -3,5 +3,5 @@ STARTDATE = "1Jan15"
 ENDDATE = "30Sep24"
 N_THRESHOLD_BPS = 100
 DATA_DIR = "./../data/"
-EVAL_START_DATE = '2019-01-01' #Eval period: this until TEST_START_DATE below
+EVAL_START_DATE = '2021-01-01' #Eval period: this until TEST_START_DATE below
 TEST_START_DATE = '2023-01-01' #until 2024-09-30 when the dataset ends
